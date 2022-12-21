c50841a337f5f19f0550333c6b3cc89f6baedd6d
Darkfall48 <sidney@sebban.be>
2022-12-21 20:37:35 +0200

    Update weather.service.js

--- scripts/services/weather.service.js
+++ scripts/services/weather.service.js
@@ -1,7 +1,7 @@
 export const weather = { callWeather }
 
 function callWeather({ lat, lng: lon }) {
-  const WEATHER_KEY = 'xxxx'
+  const WEATHER_KEY = 'c3b81f4ec3ef8ff6a1c3b96902e6b85b'
   return axios
     .get(
       `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_KEY}`
