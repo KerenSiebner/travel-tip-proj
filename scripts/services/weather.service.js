d694fdb579f0b2f87ff8f8759a107636339d7e4c
Darkfall48 <sidney@sebban.be>
2022-12-21 20:52:05 +0200

    Updated weather.service.js

--- scripts/services/weather.service.js
+++ scripts/services/weather.service.js
@@ -1,5 +1,9 @@
 export const weather = { callWeather }
 
+// const testAPI = { lat: 32.047104, lng: 34.832384 }
+
+// callWeather(testAPI).then(console.logs)
+
 function callWeather({ lat, lng: lon }) {
   const WEATHER_KEY = 'c3b81f4ec3ef8ff6a1c3b96902e6b85b'
   return axios
@@ -8,7 +12,8 @@ function callWeather({ lat, lng: lon }) {
     )
     .then((weatherData) => {
       console.log('OpenWeather API Requested')
-      return weatherData.weather
+      //   console.log(weatherData.data.weather)
+      return weatherData.data.weather
     })
     .catch((err) => {
       console.warn('HAD ERROR WITH:', err)
